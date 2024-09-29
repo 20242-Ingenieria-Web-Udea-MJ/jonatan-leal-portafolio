35e6bb20e4d0fdde04e0025d1707d6cf91528bfc
Jonatan Leal <jonatan.leal@udea.edu.co>
2024-09-28 19:06:19 -0500

    add atoms

---
+++ src/components/atoms/icons/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+
+type IconProps = {
+  icon: string;
+  url?: string;
+};
+
+const IconSocial = ({ icon, url }: IconProps) => {
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer" className="block">
+      <Icon icon={icon} className="bg-primary rounded-full h-12 w-12" />
+    </a>
+  );
+};
+
+const IconCard = ({ icon }: IconProps) => {
+  return <Icon icon={icon} className="h-12 w-12 text-primary"/>;
+};
+
+export { IconSocial, IconCard };
